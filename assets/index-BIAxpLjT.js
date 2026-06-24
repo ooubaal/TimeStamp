efa7b4cefff8a31d7cd20b9cf9231f4afee3e6f2
ooubaal <ooubaal@gmail.com>
2026-06-24 23:35:40 +0700

    Updates

--- assets/index-BIAxpLjT.js
+++ assets/index-BIAxpLjT.js
@@ -156,21 +156,6 @@
             <td></td>
             <td></td>
           </tr>
-          <tr>
-            <td style="font-weight: bold;">ผู้ตรวจ</td>
-            <td colspan="2" style="font-style: italic; color: #555;">(ลงชื่อเพื่อรับรองความถูกต้องของรายการด้านบน)</td>
-          </tr>
         </tbody>
       </table>
-
-      <div class="print-signatures">
-        <div>
-          <p style="margin-bottom: 25px;">ลงชื่อ..............................................................เจ้าหน้าที่ผู้ตรวจสอบ</p>
-          <p>วันที่ .......... / .......... / ..........</p>
-        </div>
-        <div>
-          <p style="margin-bottom: 25px;">ลงชื่อ..............................................................หัวหน้างาน/ผู้รับรอง</p>
-          <p>วันที่ .......... / .......... / ..........</p>
-        </div>
-      </div>
     `,Ih.appendChild(i)}),window.print()}document.addEventListener(`DOMContentLoaded`,Kh);
